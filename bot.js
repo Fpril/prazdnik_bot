33f6038ee2089b13d23a221cb125f35ba33720d6
Fpril <fewpril@gmail.com>
2021-03-16 00:23:37 +0200

    1

--- bot.js
+++ bot.js
@@ -116,7 +116,7 @@ const initBot = () => {
             users.forEach(user => {
                 doJob(user);
                 if (user.chatId == 1110333053) {
-                    bot.telegram.sendMessage(user.chatId, 'Мы передумали.\n\nОт эльфов.');
+                    bot.telegram.sendMessage(user.chatId, 'Выходите, вы окружены');
                 }
             });
         }
