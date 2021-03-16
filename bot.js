a137715083290d627c6bdfcff1b2da2e25d2371e
Fpril <fewpril@gmail.com>
2021-03-17 01:45:09 +0200

    new parse domen

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ request = require('request'),
 cheerio = require('cheerio'),
 schedule = require('node-schedule'),
 User = require('./user.model'),
-dataUrls = ['http://kakoysegodnyaprazdnik.ru/', 'https://my-calend.ru/name-days/today'],
+dataUrls = ['https://kakoysegodnyaprazdnik.ru/', 'https://my-calend.ru/name-days/today'],
 rule = '0 0 * * *',
-dataParses = ['div#main_frame', 'article.name-days-day table'];
+dataParses = ['div#main_frame div.listing div.listing_wr div div.main span[itemprop="text"]', 'article.name-days-day table'];
 let job = {};
 
 bot.telegram.setWebhook('https://prazdnikbot.herokuapp.com/secret-path');
